6fce8fe96f0f30927da2e72ef9ce614a31076940
saiindirat <tholusaiindira@gmail.com>
2025-05-17 20:36:24 +0000

    my commit

--- qr.rb
+++ qr.rb
@@ -1,1 +1,11 @@
 # Write your solution here!
+require "rqrcode"
+
+# Use the RQRCode::QRCode class to encode some text
+RQRCode = WIFI:T:WPA;S:My wifi network;P:supersecretpassword;
+
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = RQRCodeq.as_png({ :size => 500 })
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
